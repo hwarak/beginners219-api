a7ba376ddd4d482d64fb20630091ec183a078eac
hwarak <hwarakhada@gmail.com>
2022-10-11 22:51:24 +0900

    first commit

---
+++ src/main/java/com/beginus/beginners219/Beginners219Application.java
@@ -0,0 +1,13 @@
+package com.beginus.beginners219;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beginners219Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beginners219Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/beginus/beginners219/Beginners219ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beginus.beginners219;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beginners219ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
